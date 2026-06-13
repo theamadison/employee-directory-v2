bcf0182b1f94c1b83c24b70762c844b8d4de82c3
theamadison <alfoxthea@gmail.com>
2026-06-13 13:06:20 -0400

    Add files via upload

--- db/employees.js
+++ db/employees.js
@@ -1,3 +1,10 @@
+/**
+ * @typedef Employee
+ * @property {number} id - Unique identifier for the employee
+ * @property {string} name - Name of the employee
+ */
+
+/** @type {Employee[]} */
 const employees = [
   { id: 1, name: "Carolynn McGinlay" },
   { id: 2, name: "Lodovico Filon" },
@@ -14,14 +21,30 @@ const employees = [
 /* WARNING: this must remain the default export in order for the tests to work! */
 export default employees;
 
-export function getEmployees() {
-  return employees;
+/**
+ * Creates a new employee with the given name and appends it into the array.
+ * The ID of the created employee is one greater than the ID of the last employee.
+ * @param {string} name - the name of the new employee
+ * @returns {Employee} - the newly created employee
+ */
+export function addEmployee(name) {
+  const employee = { id: employees.length + 1, name };
+  employees.push(employee);
+  return employee;
 }
 
-export function getEmployee(id) {
+/**
+ * @param {number} id - ID of the employee to retrieve
+ * @returns {Employee} the employee with the given ID
+ * @returns {undefined} if no employee with the given ID exists
+ */
+export function getEmployeeById(id) {
   return employees.find((e) => e.id === id);
 }
 
+/**
+ * @returns {Employee} a random employee
+ */
 export function getRandomEmployee() {
   const randomIndex = Math.floor(Math.random() * employees.length);
   return employees[randomIndex];
